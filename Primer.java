f2c1e1d374bdfeb087946a77ebeee0fd1b32931f
User <cmoussafia@almata.cat>
2024-10-14 16:56:12 +0200

    Afegit Primer.java i setup.cfg

---
+++ Primer.java
@@ -0,0 +1,6 @@
+// Aquest és el primer arxiu creat i encara no he fet cap commit.
+public class Primer {
+    public static void main(String[] args) {
+        System.out.println("Hola, món!");
+    }
+}
