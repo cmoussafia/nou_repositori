0f0f96a5f2d44d98c087af164f489c427ad7d664
cmoussafia <cmoussafia@almata.cat>
2024-10-14 21:43:39 +0200

    Update Util.java
    Afegit un comentari sobre un error detectat.

--- Util.java
+++ Util.java
@@ -1,5 +1,6 @@
 // Aquest és un nou arxiu que millora el programa que fem.
 // Aquesta és una nova millora a la utilitat
+// He detectat un error que cal corregir
 
 public class Segon {
     public static void main(String[] args) {
