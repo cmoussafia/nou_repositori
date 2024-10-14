cd42712a2a24042cd2caefdd834513fcaaa9161d
User <cmoussafia@almata.cat>
2024-10-14 17:08:26 +0200

    Modificat Primer.java i afegit Segon.java

--- Primer.java
+++ Primer.java
@@ -1,4 +1,5 @@
 // Aquest és el primer arxiu creat i encara no he fet cap commit.
+// Aquest programa imprimeix "Hola, món!" a la consola.
 public class Primer {
     public static void main(String[] args) {
         System.out.println("Hola, món!");
---
+++ Segon.java
@@ -0,0 +1,6 @@
+// Aquest és un nou arxiu que millora el programa que fem.
+public class Segon {
+    public static void main(String[] args) {
+        System.out.println("Millora del programa!");
+    }
+}
