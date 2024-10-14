3fd6911bd8cebaabe9c6d0a4f8188269556274fd
User <cmoussafia@almata.cat>
2024-10-14 18:00:53 +0200

    Afegit comentari a Util.java per la nova versió de la utilitat (v2)

---
+++ Util.java
@@ -0,0 +1,5 @@
+// Aquest arxiu de codi té la funció d’ampliar el programa amb una utilitat.
+// Estic treballant en la nova versió de la utilitat (v2).
+public class Util {
+    // Aquí pots afegir mètodes i funcionalitats addicionals.
+}
